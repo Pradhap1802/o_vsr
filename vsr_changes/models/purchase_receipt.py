c54e64c790ce3481f25362581ee5f49ff5031cc1
Pradhap1802 <pradhap@processdrive.com>
2025-12-12 10:11:42 +0530

    update the Wastage Calculation in Receipts

--- vsr_changes/models/purchase_receipt.py
+++ vsr_changes/models/purchase_receipt.py
@@ -34,7 +34,7 @@ class StockPickingVSR(models.Model):
         }
 
     def _update_wastage_from_scrap(self):
-        """Update wastage field based on manually entered scrap records"""
+        """Update wastage field and deduct from next transfer's demand based on scrap records"""
         for picking in self:
             # Search for all scrap records linked to this picking
             scrap_records = self.env['stock.scrap'].search([
@@ -42,45 +42,41 @@ class StockPickingVSR(models.Model):
             ])
             
             if scrap_records:
+                # Group wastage by product
+                wastage_by_product = {}
+                for scrap in scrap_records:
+                    product_id = scrap.product_id.id
+                    if product_id not in wastage_by_product:
+                        wastage_by_product[product_id] = 0
+                    wastage_by_product[product_id] += scrap.scrap_qty
+                
                 # Sum up all manually entered scrap quantities
                 total_scrap_qty = sum(scrap.scrap_qty for scrap in scrap_records)
                 
                 # Update the wastage field in this picking
                 picking.wastage = total_scrap_qty
                 
-                # Also update wastage in all downstream transfers
-                self._update_downstream_wastage(picking, total_scrap_qty)
-
-    def _update_downstream_wastage(self, picking, wastage_qty):
-        """Update wastage in all downstream (next) transfers and deduce demand/quantity"""
-        # Find all moves from this picking that have destination moves
-        destination_moves = picking.move_ids.mapped('move_dest_ids')
-        
-        if destination_moves:
-            # Get the pickings that contain these destination moves
-            next_pickings = destination_moves.mapped('picking_id')
-            
-            for next_picking in next_pickings:
-                if next_picking.state == 'done':
-                    # Update wastage in next picking
-                    next_picking.wastage = wastage_qty
+                # Update next transfer's demand by deducting wastage
+                destination_moves = picking.move_ids.mapped('move_dest_ids')
+                
+                if destination_moves:
+                    # Get the pickings that contain these destination moves
+                    next_pickings = destination_moves.mapped('picking_id')
                     
-                    # Deduct wastage from demand and quantity in the next transfer
-                    for move in next_picking.move_ids:
-                        # Reduce demand (product_uom_qty) by wastage amount
-                        if move.product_uom_qty > wastage_qty:
-                            move.product_uom_qty -= wastage_qty
-                        else:
-                            move.product_uom_qty = 0
-                        
-                        # Reduce quantity done (qty_done) by wastage amount if manually entered
-                        if move.quantity_done > wastage_qty:
-                            move.quantity_done -= wastage_qty
-                        else:
-                            move.quantity_done = 0
-                    
-                    # Continue to next level (recursive)
-                    self._update_downstream_wastage(next_picking, wastage_qty)
+                    for next_picking in next_pickings:
+                        # Deduct wastage from demand in the next transfer
+                        for move in next_picking.move_ids:
+                            if move.product_id.id in wastage_by_product:
+                                wastage_qty = wastage_by_product[move.product_id.id]
+                                
+                                # Reduce demand (product_uom_qty) by wastage amount
+                                if move.product_uom_qty >= wastage_qty:
+                                    move.product_uom_qty -= wastage_qty
+                                else:
+                                    move.product_uom_qty = 0
+                                
+                                # Also update quantity to match demand
+                                move.quantity = move.product_uom_qty
 
 
 class StockScrapVSR(models.Model):
